70a60abe4ce5401d5d83b65df4e40127977472d7
Dániel Vince <vinced@inf.u-szeged.hu>
2023-10-16 15:45:59 +0200

    Make logger event handler compatible with Picireny

--- picire/events/logger.py
+++ picire/events/logger.py
@@ -10,36 +10,38 @@ import logging
 from picire.outcome import Outcome
 from .events import EventHandler
 
-logger = logging.getLogger(__name__)
-
 
 class Logger(EventHandler):
 
+    def __init__(self, logger = None) -> None:
+        super().__init__()
+        self.logger = logger or logging.getLogger(__name__)
+
     def iteration_started(self, iteration: int, **kwargs) -> None:
-        logger.info(f'Iteration {iteration}')
+        self.logger.info(f'Iteration {iteration}')
 
     def cycle_started(self, cycle: int, configuration: list, **kwargs) -> None:
-        logger.info(f'Run {cycle}')
-        logger.info(f'\t Config size: {len(configuration)}')
+        self.logger.info(f'Run {cycle}')
+        self.logger.info(f'\t Config size: {len(configuration)}')
 
     def finished(self, reason: str, result: str) -> None:
-        logger.info(f'\t Stopped: {reason}')
-        logger.info(f'\t Size of the result: {len(result)}')
+        self.logger.info(f'\t Stopped: {reason}')
+        self.logger.info(f'\t Size of the result: {len(result)}')
 
     def succesful_reduction(self, configuration: list) -> None:
-        logger.info(f'\t Reduced to: {len(configuration)}')
+        self.logger.info(f'\t Reduced to: {len(configuration)}')
 
     def configuration_split(self, **kwargs) -> None:
-        logger.info('\t Increased granularity')
+        self.logger.info('\t Increased granularity')
 
     def test_started(self, configuration_id: str, **kwargs) -> None:
-        logger.debug(f'\t [{configuration_id}]: test...')
+        self.logger.debug(f'\t [{configuration_id}]: test...')
 
     def test_finished(self, configuration_id: str, outcome: Outcome, **kwargs) -> None:
-        logger.debug(f'\t [{configuration_id}]: test = {outcome.name}')
+        self.logger.debug(f'\t [{configuration_id}]: test = {outcome.name}')
 
     def cache_lookup(self, configuration_id: str, outcome: Outcome, **kwargs) -> None:
-        logger.debug(f'\t [{configuration_id}]: cache = {outcome.name}')
+        self.logger.debug(f'\t [{configuration_id}]: cache = {outcome.name}')
 
     def cache_insert(self,
                      configuration_id: str,
@@ -47,4 +49,4 @@ class Logger(EventHandler):
                      size: int,
                      length: int,
                      **kwargs) -> None:
-        logger.debug(f'\t [{configuration_id}]: cache => {outcome.name} (cache: {length} items, {size} bytes)')
+        self.logger.debug(f'\t [{configuration_id}]: cache => {outcome.name} (cache: {length} items, {size} bytes)')
